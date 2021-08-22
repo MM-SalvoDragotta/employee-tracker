2452e72a46995206d9cf54e51383f3d6ab9937ea
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-08-22 19:42:21 +1000

    Fix connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const util = require("util");
+const colors = require('colors');
+const figlet = require('figlet');
+const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -7,7 +10,26 @@ const db = mysql.createConnection({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME  
 },
-console.log(`Connected to the employees_db database.`)
+console.log(`Connected to the employees_db database.` .bgBlue)
 );
+
+// Connect to the DB
+db.connect((err) => {
+  if (err) throw err;   
+  figlet.text('Employee  Tracker' , 
+  {
+      verticalLayout: 'full'  
+  },
+ (err, data) => {
+    if (err) {
+      console.log('ascii art not loaded');
+    } else {
+      console.log(data);
+    }  
+  //   startPrompt();
+  });
+});
+
+db.query = util.promisify(db.query);
 
 module.exports = db;
--- index.js
+++ index.js
@@ -10,30 +10,7 @@ const figlet = require('figlet');
 require('dotenv').config();
 const util = require("util");
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME  
-  },
-  console.log(`Connected to the employees_db database.` .bgBlue)
-  );
+const db = require('./config/connection')
 
-// Connect to the DB
-db.connect((err) => {
-    if (err) throw err;   
-    figlet.text('Employee  Tracker' , 
-    {
-        verticalLayout: 'full'  
-    },
-   (err, data) => {
-      if (err) {
-        console.log('ascii art not loaded');
-      } else {
-        console.log(data);
-      }  
-    //   startPrompt();
-    });
-  });
 
-  db.query = util.promisify(db.query);
+
