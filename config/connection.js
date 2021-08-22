4a140e4e9cd61956a2925dbabb65f77c9fd6870d
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-08-22 20:09:21 +1000

    Change logo to asciiart-logo

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ const util = require("util");
 const colors = require('colors');
 const figlet = require('figlet');
 const mysql = require('mysql2');
+const logo = require('asciiart-logo');
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -14,19 +15,49 @@ console.log(`Connected to the employees_db database.` .bgBlue)
 );
 
 // Connect to the DB
+// db.connect((err) => {
+//   if (err) throw err;   
+//   figlet.text('Employee  Tracker' , 
+//   {
+//       verticalLayout: 'full'  
+//   },
+//  (err, data) => {
+//     if (err) {
+//       console.log('logo not loaded');
+//     } else {
+//       console.log(data);
+//     }  
+//   });
+// });
+
+const longText = `
+Welcome to Employee Tracker. This  is a command-line application to manage 
+a company's employee database, using Node.js, Inquirer, and MySQL.`
+
 db.connect((err) => {
   if (err) throw err;   
-  figlet.text('Employee  Tracker' , 
-  {
-      verticalLayout: 'full'  
-  },
+  console.log(
+    logo({
+      name: 'Employee Tracker',
+      font: 'Standard',
+      lineChars: 10,
+      padding: 2,
+      margin: 3,
+      borderColor: 'grey',
+      logoColor: 'bold-green',
+      textColor: 'green',
+    })
+    .emptyLine()
+    .right('version 1.0.0')
+    .emptyLine()
+    .center(longText)
+    .render(),
  (err, data) => {
     if (err) {
-      console.log('ascii art not loaded');
+      console.log('logo not loaded');
     } else {
-      console.log(data);
+      // console.log(data);
     }  
-  //   startPrompt();
   });
 });
 
