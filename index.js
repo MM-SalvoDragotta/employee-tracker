d49495d209cf6c67e450853777abbd905f4e3cbe
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-08-22 19:45:56 +1000

    Add asciiart-logo

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const colors = require('colors');
 const validateEmail = require('email-validator');
 const consoleTable = require("console.table");
 const mysql = require('mysql2');
-const figlet = require('figlet');
-require('dotenv').config();
-const util = require("util");
-
 const db = require('./config/connection')
 
 
