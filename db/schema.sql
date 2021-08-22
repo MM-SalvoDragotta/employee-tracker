c91b85b98a7f64a9a12ffd405a0c12d0e7b3510d
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-08-22 17:24:58 +1000

    Minor Changes to Schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,21 @@
-DROP DATABASE IF EXISTS employees_DB;
-CREATE database employees_DB;
+DROP DATABASE IF EXISTS employees_db;
+CREATE database employees_db;
 
-USE employees_DB;
+USE employees_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
+  title VARCHAR(30) UNIQUE NOT NULL,
   salary DECIMAL(10, 0) NULL,
   department_id INT NOT NULL,
   FOREIGN KEY (department_id)
         REFERENCES department(id)
-        ON DELETE SET NULL
+        ON DELETE CASCADE 
         ON UPDATE CASCADE
 );
 
@@ -26,10 +26,10 @@ CREATE TABLE employee (
   role_id INT NOT NULL,
   manager_id INT NULL,
   FOREIGN KEY (role_id)
-        REFERENCES roles(id)
-        ON DELETE SET NULL
+        REFERENCES role(id)
+        ON DELETE CASCADE,
   FOREIGN KEY (manager_id)
         REFERENCES employee(id)
-        ON DELETE SET NULL
+        ON DELETE CASCADE 
         ON UPDATE CASCADE         
 );
---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+USE employees_DB;
+
+INSERT INTO department (name)
+VALUES 
+    ("Engineering"),
+    ("Finance"),
+    ("Legal"),
+    ("Sales");
+
+SELECT * FROM department;
+
+-- -- engineering department has engineers and project managers and engineering manager
+-- -- finance department has accountants and managers
+-- --  marketing department has product marketing manager, marketing lead
+-- INSERT INTO roles (title, salary, department_id)
+-- VALUES ("Sales Lead", 100000, 1),
+--     ("project manager", 90000, 1),
+--     ("engineering manager", 225000, 1),
+--     ("accountant", 70000, 2),
+--     ("accounting manager", 120000, 2),
+--     ("product marketing manager", 50000, 3),
+--     ("marketing lead", 150000, 3),
+--     ("sales rep", 85000, 4);
+
+-- SELECT * FROM ROLE;
+
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id)
+-- VALUES ("Spike", "Bulldog", 3, NULL),
+--     ("Tom", "Cat", 3, 1),
+--     ("Jerry", "Mouse", 1, 2),
+--     ("Mickey", "Mouse", 1, 2),
+--     ("Toots", "Mouse", 2, 1);
+
+-- SELECT * FROM employee;
